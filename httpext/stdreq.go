4cc4220a7dd698fbbe084bd15d4cb7b2ae3fab31
CodapeWild <codape.wild@gmail.com>
2021-08-05 17:16:55 +0800

    modify RemoteIp

--- httpext/stdreq.go
+++ httpext/stdreq.go
@@ -13,17 +13,16 @@ import (
 
 func RemoteIp(req *http.Request) (ip, port string) {
 	var err error
+BREAKPOINT:
 	for _, h := range []string{"x-forwarded-for", "x-real-ip", "proxy-client-ip"} {
 		addrs := strings.Split(req.Header.Get(h), ",")
-		for i := len(addrs) - 1; i >= 0; i-- {
-			if ip, port, err = net.SplitHostPort(addrs[i]); err != nil || ip == "" {
+		for _, addr := range addrs {
+			if ip, port, err = net.SplitHostPort(addr); err != nil || ip == "" {
 				continue
-			} else {
-				goto FOUND
 			}
+			break BREAKPOINT
 		}
 	}
-FOUND:
 	if ip == "" {
 		ip, port, _ = net.SplitHostPort(req.RemoteAddr)
 	}
