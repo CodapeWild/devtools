a2077775b9d5567ee6f05cfc9ebc641f9f75c017
CodapeWild <codape.wild@gmail.com>
2021-08-09 10:27:07 +0800

    fix

--- httpext/stdreq.go
+++ httpext/stdreq.go
@@ -11,13 +11,12 @@ import (
 	"github.com/CodapeWild/devtools/comerr"
 )
 
-func RemoteIp(req *http.Request) (ip, port string) {
-	var err error
+func RemoteAddr(req *http.Request) (ip, port string) {
 BREAKPOINT:
 	for _, h := range []string{"x-forwarded-for", "X-FORWARDED-FOR", "X-Forwarded-For", "x-real-ip", "X-REAL-IP", "X-Real-Ip", "proxy-client-ip", "PROXY-CLIENT-IP", "Proxy-Client-Ip"} {
 		addrs := strings.Split(req.Header.Get(h), ",")
 		for _, addr := range addrs {
-			if ip, port, err = net.SplitHostPort(addr); err != nil || ip == "" {
+			if ip, port, _ = net.SplitHostPort(addr); ip == "" {
 				continue
 			}
 			break BREAKPOINT
