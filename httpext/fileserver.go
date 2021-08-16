1a4689a3767f70e00a2e640db9de8ad7c0468c25
CodapeWild <codape.wild@gmail.com>
2021-08-16 15:47:45 +0800

    add httpext upload handler

--- httpext/fileserver.go
+++ httpext/fileserver.go
@@ -1,14 +1,54 @@
 package httpext
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path"
+	"time"
+)
+
+const (
+	upload_file_header = "upload_file_field"
+)
+
+const (
+	root_dir             = "./upload"
+	max_mem_usage        = 10 << 20
+	max_upload_file_size = 100 << 20
+)
+
+var (
+	ErrFileSizeTooBig = errors.New("file size too large")
+	ErrInvalidMIME    = errors.New("invalid file type")
+)
+
+var (
+	defMkDir = func(req *http.Request, root string) (string, error) {
+		dir := path.Join(root, time.Now().Local().Format("2006-01-02"))
+
+		return dir, os.MkdirAll(dir, 0755)
+	}
+	defWriteFile = func(dir, filename, extension string, buf []byte) error {
+		return os.WriteFile(fmt.Sprintf("%s%c%s.%s", dir, os.PathSeparator, filename, extension), buf, 0755)
+	}
 )
 
 type FileServer struct {
 	http.ServeMux
 	http.FileSystem
-	root    string
-	listDir bool
+	root              string
+	listDir           bool
+	uploadFileHeader  string
+	maxMemoryUsage    int64
+	maxUploadFileSize int64
+	validContentTypes map[string]string
+	mkdir             func(req *http.Request, root string) (string, error)
+	writefile         func(dir, filename, extension string, buf []byte) error
 }
 
 type Option func(fsrv *FileServer)
@@ -19,32 +59,83 @@ func ConfigRootDir(root string) Option {
 	}
 }
 
-func ConfigIsListDir(list bool) Option {
+func ConfigListDir(list bool) Option {
 	return func(fsrv *FileServer) {
 		fsrv.listDir = list
 	}
 }
 
-func RegisterUploadPattern(path string) Option {
+func ConfigUploadFileHeader(field string) Option {
 	return func(fsrv *FileServer) {
-		fsrv.Handle(path, http.HandlerFunc(fsrv.Upload))
+		fsrv.uploadFileHeader = field
 	}
 }
 
-func RegisterDownloadPattern(path string) Option {
+func ConfigMaxMemoryUsage(max int64) Option {
 	return func(fsrv *FileServer) {
-		fsrv.Handle(path, http.HandlerFunc(fsrv.Download))
+		fsrv.maxMemoryUsage = max
 	}
 }
 
-func RegisterOpenPattern(path string) Option {
+func ConfigMaxUploadFileSize(max int64) Option {
 	return func(fsrv *FileServer) {
-		fsrv.Handle(path, http.StripPrefix(path, http.FileServer(fsrv)))
+		fsrv.maxUploadFileSize = max
+	}
+}
+
+func ConfigValidContentTypes(types map[string]string) Option {
+	return func(fsrv *FileServer) {
+		fsrv.validContentTypes = types
+	}
+}
+
+func ConfigMkDirFunc(mkdir func(req *http.Request, root string) (string, error)) Option {
+	return func(fsrv *FileServer) {
+		fsrv.mkdir = mkdir
+	}
+}
+
+func ConfigWriteFileFunc(writefile func(dir, filename, extension string, buf []byte) error) Option {
+	return func(fsrv *FileServer) {
+		fsrv.writefile = writefile
+	}
+}
+
+func RegisterUploadPattern(pattern string, middleware MiddlewareFunc) Option {
+	return func(fsrv *FileServer) {
+		if middleware == nil {
+			fsrv.HandleFunc(pattern, fsrv.Upload)
+		} else {
+			fsrv.Handle(pattern, middleware(http.HandlerFunc(fsrv.Upload)))
+		}
+	}
+}
+
+func RegisterDownloadPattern(pattern string, middleware MiddlewareFunc) Option {
+	return func(fsrv *FileServer) {
+		if middleware == nil {
+			fsrv.HandleFunc(pattern, fsrv.Download)
+		} else {
+			fsrv.Handle(pattern, middleware(http.HandlerFunc(fsrv.Download)))
+		}
+	}
+}
+
+func RegisterOpenPattern(pattern string) Option {
+	return func(fsrv *FileServer) {
+		fsrv.Handle(pattern, http.StripPrefix(pattern, http.FileServer(fsrv)))
 	}
 }
 
 func NewFileServer(opts ...Option) *FileServer {
-	fsrv := &FileServer{}
+	fsrv := &FileServer{
+		root:              root_dir,
+		uploadFileHeader:  upload_file_header,
+		maxMemoryUsage:    max_mem_usage,
+		maxUploadFileSize: max_upload_file_size,
+		mkdir:             defMkDir,
+		writefile:         defWriteFile,
+	}
 	for _, opt := range opts {
 		opt(fsrv)
 	}
@@ -53,12 +144,86 @@ func NewFileServer(opts ...Option) *FileServer {
 }
 
 func (this *FileServer) Upload(resp http.ResponseWriter, req *http.Request) {
-	err := req.ParseMultipartForm(10 << 20)
+	err := req.ParseMultipartForm(this.maxMemoryUsage)
 	if err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
 
+		return
 	}
-	http.DefaultTransport
-	req.FormFile()
+
+	mulf, mulh, err := req.FormFile(this.uploadFileHeader)
+	if err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	if mulh.Size > this.maxUploadFileSize {
+		log.Println(ErrFileSizeTooBig.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	first512 := make([]byte, 512)
+	if _, err := mulf.Read(first512); err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+	mimestr := http.DetectContentType(first512)
+
+	var ext string
+	if len(this.validContentTypes) != 0 {
+		var ok bool
+		if ext, ok = this.validContentTypes[mimestr]; !ok {
+			log.Println(ErrInvalidMIME.Error())
+			resp.WriteHeader(http.StatusBadRequest)
+
+			return
+		}
+	} else {
+		if extensions, err := mime.ExtensionsByType(mimestr); err != nil {
+			log.Println(err.Error())
+			resp.WriteHeader(http.StatusBadRequest)
+
+			return
+		} else {
+			ext = extensions[0]
+		}
+	}
+
+	if this.mkdir == nil {
+		this.mkdir = defMkDir
+	}
+	dir, err := this.mkdir(req, this.root)
+	if err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	buf, err := io.ReadAll(mulf)
+	if err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+
+		return
+	}
+
+	if this.writefile == nil {
+		this.writefile = defWriteFile
+	}
+	if err = this.writefile(dir, mulh.Filename, ext, buf); err != nil {
+		log.Println(err.Error())
+		resp.WriteHeader(http.StatusBadRequest)
+	} else {
+		resp.WriteHeader(http.StatusOK)
+	}
 }
 
 func (this *FileServer) Download(resp http.ResponseWriter, req *http.Request) {
