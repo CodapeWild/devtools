bd1c8dbe20b70654a00de646e146355063910ebc
CodapeWild <codape.wild@gmail.com>
2021-08-24 16:41:20 +0800

    add httpext/client.go

--- httpext/client.go
+++ httpext/client.go
@@ -1,65 +1,51 @@
 package httpext
 
 import (
-	"context"
-	"log"
 	"net"
 	"net/http"
 	"net/url"
-	"sync"
 	"time"
-
-	"github.com/CodapeWild/devtools/msgque"
 )
 
 var (
-	getProxyUrlOnce sync.Once
-	getProxyURL     func(*http.Request) (*url.URL, error)
-	defTransport    *http.Transport = &http.Transport{
-		Proxy: getProxyURL,
+	defTransport *http.Transport = &http.Transport{
 		DialContext: (&net.Dialer{
 			Timeout:   30 * time.Second,
 			KeepAlive: 30 * time.Second,
 		}).DialContext,
-		MaxIdleConns:          100,
-		MaxIdleConnsPerHost:   10,
-		IdleConnTimeout:       60 * time.Second,
+		MaxIdleConns:          300,
+		MaxIdleConnsPerHost:   20,
+		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	}
-	maxConcurrency uint = 10
-	newTicketOnce  sync.Once
-	tkque          msgque.TicketQueue
 )
 
-func UseProxy(addr string) {
-	getProxyUrlOnce.Do(func() {
-		if pxurl, err := url.ParseRequestURI(addr); err != nil {
-			log.Printf("parse proxy address failed err:%s\n", err.Error())
-		} else {
-			getProxyURL = func(r *http.Request) (*url.URL, error) {
-				return pxurl, nil
-			}
-		}
-	})
+type ClntOption func(clnt *http.Client)
+
+func WithTimeout(timeout time.Duration) ClntOption {
+	return func(clnt *http.Client) {
+		clnt.Timeout = timeout
+	}
 }
 
-func SetMaxConcurrency(max uint) {
-	maxConcurrency = max
+func WithCookies(u *url.URL, cookies []*http.Cookie) ClntOption {
+	return func(clnt *http.Client) {
+		clnt.Jar.SetCookies(u, cookies)
+	}
 }
 
-func SendRequest(ctx context.Context, req *http.Request) (resp *http.Response, err error) {
-	newTicketOnce.Do(func() {
-		tkque = msgque.NewSimpleTicketQueue(int(maxConcurrency))
-	})
+func WithTransport(transport *http.Transport) ClntOption {
+	return func(clnt *http.Client) {
+		clnt.Transport = transport
+	}
+}
 
-	go func(ticket msgque.Ticket, ctx context.Context, req *http.Request) {
-		var timeout time.Duration
-		if deadline, ok := ctx.Deadline(); ok {
-			timeout = deadline.Sub(time.Now())
-		}
+func SendRequest(req *http.Request, opts ...ClntOption) (*http.Response, error) {
+	clnt := &http.Client{Transport: defTransport}
+	for _, opt := range opts {
+		opt(clnt)
+	}
 
-		(&http.Client{Transport: defTransport, Timeout: timeout}).Do(req)
-		tkque.Restore(ticket)
-	}(tkque.Fetch(), ctx, req)
+	return clnt.Do(req)
 }
